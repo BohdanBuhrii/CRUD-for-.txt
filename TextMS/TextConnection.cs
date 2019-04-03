e306ff76ff0bcee1538647dea63524b4e246648a
BohdanBuhrii <bobuhrii@gmail.com>
2019-04-03 12:24:39 +0300

    Added classes TextConnection and TextDataReader (both without realization)

--- TextMS/TextConnection.cs
+++ TextMS/TextConnection.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TextMS
 {
-    class TextConnection
+    class TextConnection //TODO
     {
+        private readonly string ConnectionString;
+
+        public TextConnection(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public TextDataReader GetDataReader()
+        {
+            return new TextDataReader();
+        }
+
+        public class TextDataReader
+        {
+            public StreamReader streamReader;
+
+            TextDataReader(string connectionString)
+            {
+                streamReader = new StreamReader(connectionString);
+            }
+
+            public bool Read()
+            {
+                return true;
+            }
+
+            public string this[string column]
+            {
+                set { }
+                get
+                {
+                    return "";
+                }
+            }
+
+
+        }
+
+        public void Create() { }
+        public void Read<T>()
+        {
+
+        }
+        public void Update() { }
+        public void Delete() { }
     }
 }
